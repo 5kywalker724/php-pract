7e5dae03a5c73074ceae076cd70745b53ba94366
PinT271 <olezhka.kuzmin.99@inbox.ru>
2024-03-27 10:37:18 +0700

    Добавление обработки данных запроса

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -1,14 +1,15 @@
 <?php
 namespace Controller;
 
+use Src\Request;
 use Model\Post;
 use Src\View;
 
 class Site
 {
-    public function index(): string
+    public function index(Request $request): string
     {
-        $posts = Post::all();
+        $posts = Post::where('id', $request->id)->get();
         return (new View())->render('site.post', ['posts' => $posts]);
     }
 
