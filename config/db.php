3e0bf622bcc3235d4316338d3a396f218ddb2d4a
PinT271 <olezhka.kuzmin.99@inbox.ru>
2024-03-31 21:22:58 +0700

    Интеграция разработанного пакета для работы с массивами

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 return [
     'driver' => 'mysql',
     'host' => 'localhost',
-    'database' => 'vtjxjcpu_m6',
-    'username' => 'vtjxjcpu',
-    'password' => '3tJyVp',
+    'database' => 'mvc',
+    'username' => 'root',
+    'password' => '',
     'charset' => 'utf8',
     'collation' => 'utf8_unicode_ci',
     'prefix' => '',
