428410c72d93b165e3c6bbc096935be82f738eb9
Олег Кузьмин <gr412_kuoig@lab-407-5.tomtit-tomsk.ru>
2024-04-02 17:31:29 +0700

    Реализация API приложения

--- core/Src/View.php
+++ core/Src/View.php
@@ -56,6 +56,15 @@ class View
         throw new Exception('Error render');
     }
 
+    public function toJSON(array $data = [], int $code = 200)
+    {
+        header_remove();
+        header("Content-Type: application/json; charset=utf-8");
+        http_response_code($code);
+        echo json_encode($data, JSON_UNESCAPED_UNICODE);
+        exit();
+    }
+
     public function __toString(): string
     {
         return $this->render($this->view, $this->data);
